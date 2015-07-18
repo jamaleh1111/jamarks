ea788b13a3e9e62e1c78bbed2249bd95dd082a2c
Jamaleh <Jamaleh@hotmail.com>
2015-07-18 15:14:33 +1000

    create link to user profile

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,11 @@
 class LikesController < ApplicationController
 
   def create
-    @bookmark = Bookmark.find(params[:bookmark_id])
+    bookmark = Bookmark.find(params[:bookmark_id])
     like = current_user.likes.build(bookmark: @bookmark)
 
     authorize @like
+
     if like.save
       flash[:notice] = "You liked this bookmark!"
       redirect_to @bookmark
@@ -15,10 +16,11 @@ class LikesController < ApplicationController
   end 
 
   def destroy
-    @bookmark = Bookmark.find(params[:bookmark_id])
+    bookmark = Bookmark.find(params[:bookmark_id])
     like = current_user.likes.find(params[:id])
 
     authorize @like
+    
     if like.destroy
       flash[:notice] = "You unliked this bookmark."
       redirect_to @bookmark
