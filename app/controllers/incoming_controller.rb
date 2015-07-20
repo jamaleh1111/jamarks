a63b34f11c8b82640aab8e32225a9eade575a5f7
Jamaleh <Jamaleh@hotmail.com>
2015-07-20 21:23:21 +1000

    testing mailgun

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,12 +5,28 @@ class IncomingController < ApplicationController
   def create
 
     @user = User.where(email: params['sender']).first
+    if @user
+      puts "we have user"
+    else 
+      puts "no user"
+    end 
 
     @topic = Topic.where(title: params[:subject]).first_or_create # subject
+    if @topic 
+      puts "we have topic"
+    else
+      puts "no  topic"
+    end 
 
     if @user
+      puts "about to create bookmark"
      @bookmark = @topic.bookmarks.create(url: params['stripped-text'], user_id: @user.id) # body of email
     
+    end 
+    if @bookmark 
+      puts "created bookmark #{@bookmark.url}" 
+    else
+      puts "no bookmark created"
     end 
 
     head 200 
