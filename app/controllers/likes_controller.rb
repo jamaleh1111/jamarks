160a3a31d3233677ef550d5b9cb529fa9fa0f420
Jamaleh <Jamaleh@hotmail.com>
2015-07-22 19:37:13 +1000

    add like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,28 @@
 class LikesController < ApplicationController
 
   def create
-    
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.build(bookmark: @bookmark)
+
+    authorize @like
+    if like.save
+      flash[:notice] = "You like this bookmark!"
+    else
+      flash[:error] = "There was an error, please try again."
+    end
+    redirect_to @bookmark
   end 
 
   def destroy
-    
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.find(params[:id])
+
+    authorize @like
+    if like.destroy
+      flash[:notice] = "You do not like this bookmark?"
+    else
+      flash[:error] = "There was an error, please try again."
+    end 
+    redirect_to @bookmark
   end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-    has_many :topics
-    # has_many :likes, dependent: :destroy
-    has_many :bookmarks
+  has_many :topics
+  has_many :likes, dependent: :destroy
+  has_many :bookmarks # dependent destroy?
 
-    def admin?
-      role == 'admin'
-    end 
+  def admin?
+    role == 'admin'
+  end 
 
-    # def liked(bookmark)
-    #   likes.where(bookmark_id: bookmark.id).first
-    # end 
+  def liked(bookmark)
+    likes.where(bookmark_id: bookmark.id).first
+  end 
+  
 end
